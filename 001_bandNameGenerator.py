c3d5cd3466ae3a236ec9d440f0bd80bedf5aa950
Ivo Yordanov <lalanon@gmail.com>
2021-09-19 10:35:47 +0300

    First day done!

---
+++ 001_bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("=== Band Name Gen v0.1 ===")
+city = input("What's the name og your current city: ")
+animal = input("What's your favourite animal: ")
+print("Your band name is: " + city +" " + animal)
